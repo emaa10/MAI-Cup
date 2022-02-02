65849647f63b848a501bc23253dfe4c5d129756b
Emanuel Berger <94300384+emaa10@users.noreply.github.com>
2022-02-02 19:24:05 +0100

    Update code_logik_aktuell.ino

--- code_logik_aktuell.ino
+++ code_logik_aktuell.ino
@@ -1,1 +1,236 @@
-sus
+#include <Arduino.h>
+
+/*
+  MAI Cup Junior Code
+  made at 25 Jan 2022
+  by Christoph & Emanuel @ MAI Robotics
+  Home
+  26.01.2022
+*/
+
+//Todos: Zeit für Kurve einfügen, Pins einstellen, Links und rechts verkabeln, Besser Verkabeln
+// - Pins -
+// Motor rechts
+#define RIGHT_RPWM 5
+#define RIGHT_LPWM 6
+#define RIGHT_REN 8
+#define RIGHT_LEN 9
+//Motor links
+#define LEFT_RPWM 10
+#define LEFT_LPWM 11
+#define LEFT_REN 2
+#define LEFT_LEN 3
+// Ultraschall vorne
+#define TRIGGER_VORNE 12 //Ultraschallsensor vorne Trigger Pin 
+#define ECHO_VORNE 13    //Ultraschallsensor vorne Echo Pin 
+// Ultraschall links
+#define TRIGGER_LINKS 7 
+#define ECHO_LINKS A5
+//Ultraschall rechts
+#define TRIGGER_RECHTS 4 
+#define ECHO_RECHTS A4
+
+// - Daten -
+int outLeft; 
+int outRight;
+// alte trash logic dinger
+int logicRight; //Temporärer Speicher bei der Kurve
+int logicLeft; //Temporärer Speicher bei der Kurve
+int logicRight1; //Temporärer Speicher bei der Kurve
+int logicLeft1; //Temporärer Speicher bei der Kurve
+int logicRight2; //Temporärer Speicher bei der Kurve
+int logicLeft2; //Temporärer Speicher bei der Kurve
+// neue logic checks
+int hindernisLinks;
+int hindernisRechts;
+
+long dauerVorne=0; // Dauer Speicher für Ultraschcallsensor vorne
+long entfernungVorne=0; // Entfernung Speicher für Ultraschcallsensor vorne
+
+long dauerLinks=0; 
+long entfernungLinks=0; 
+
+long dauerRechts=0; 
+long entfernungRechts=0;
+
+void motorAnsteuern() {
+  analogWrite(RIGHT_LPWM,outRight); //Schreibe Geschwindigkeit auf Pins
+  analogWrite(RIGHT_RPWM,0);        //Schreibe Geschwindigkeit auf Pins
+  analogWrite(LEFT_LPWM,outLeft);   //Schreibe Geschwindigkeit auf Pins
+  analogWrite(LEFT_RPWM,0);         //Schreibe Geschwindigkeit auf Pins
+}
+
+/*
+ * Motoren starten (beiden fahren)
+ */
+void fahrenBeide() {
+  outLeft = 100; //Setze Geschwindigkeit links auf 100
+  outRight = 100; //Setze Geschwindigkeit rechts auf 100
+  motorAnsteuern();
+  Serial.println("fahren beide laut Methode");
+}
+
+/*
+ * Motoren stoppen (beide)
+ */
+void stehenbleiben() {
+  outLeft = 0;
+  outRight = 0;
+  motorAnsteuern();
+  Serial.println("stehen beide laut Variable");
+}
+
+/*
+ * Methode zum Messen der Entfernung
+ * @todo: links und rechts wieder einkommentieren
+ */
+void entfernungMessenVorne() {
+  digitalWrite(TRIGGER_VORNE, LOW); //Hier nimmt man die Spannung für kurze Zeit vom Trigger-Pin, damit man später beim Senden des Trigger-Signals ein rauschfreies Signal hat.
+  delay(5); // Pause 5 Millisekunden
+  digitalWrite(TRIGGER_VORNE, HIGH); //Jetzt sendet man eine Ultraschallwelle los.
+  delay(10); //Dieser „Ton“ erklingt für 10 Millisekunden.
+  digitalWrite(TRIGGER_VORNE, LOW);//Dann wird der „Ton“ abgeschaltet.
+  dauerVorne = pulseIn(ECHO_VORNE, HIGH); //Mit dem Befehl „pulseIn“ zählt der Mikrokontroller die Zeit in Mikrosekunden, bis der Schall zum Ultraschallsensor zurückkehrt.
+  entfernungVorne = (long)((dauerVorne/2) * 0.03432); //Nun berechnet man die Entfernung in Zentimetern. Man teilt zunächst die Zeit durch zwei (Weil man ja nur eine Strecke berechnen möchte und nicht die Strecke hin- und zurück). Den Wert multipliziert man mit der Schallgeschwindigkeit in der Einheit Zentimeter/Mikrosekunde und erhält dann den Wert in Zentimetern.
+  
+  if (entfernungVorne >= 500 || entfernungVorne <= 0) {//Wenn die gemessene Entfernung über 500cm oder unter 0cm liegt,…
+    Serial.println("Kein Messwert"); //dann soll der serial monitor ausgeben „Kein Messwert“, weil Messwerte in diesen Bereichen falsch oder ungenau sind.
+  }
+  else {
+    Serial.print(entfernungVorne); //…soll der Wert der Entfernung an den serial monitor hier ausgegeben werden.
+    Serial.println(" cm Vorne"); // Hinter dem Wert der Entfernung soll auch am Serial Monitor die Einheit "cm" angegeben werden, danach eine neue Zeile
+  }
+}
+
+void entfernungMessenLinks() {
+  digitalWrite(TRIGGER_LINKS, LOW); //Hier nimmt man die Spannung für kurze Zeit vom Trigger-Pin, damit man später beim Senden des Trigger-Signals ein rauschfreies Signal hat.
+  delay(5); // Pause 5 Millisekunden
+  digitalWrite(TRIGGER_LINKS, HIGH); //Jetzt sendet man eine Ultraschallwelle los.
+  delay(10); //Dieser „Ton“ erklingt für 10 Millisekunden.
+  digitalWrite(TRIGGER_LINKS, LOW);//Dann wird der „Ton“ abgeschaltet.
+  dauerLinks = pulseIn(ECHO_LINKS, HIGH); //Mit dem Befehl „pulseIn“ zählt der Mikrokontroller die Zeit in Mikrosekunden, bis der Schall zum Ultraschallsensor zurückkehrt.
+  entfernungLinks = (long)((dauerLinks/2) * 0.03432); //Nun berechnet man die Entfernung in Zentimetern. Man teilt zunächst die Zeit durch zwei (Weil man ja nur eine Strecke berechnen möchte und nicht die Strecke hin- und zurück). Den Wert multipliziert man mit der Schallgeschwindigkeit in der Einheit Zentimeter/Mikrosekunde und erhält dann den Wert in Zentimetern.
+  
+  if (entfernungLinks >= 500 || entfernungLinks <= 0) {//Wenn die gemessene Entfernung über 500cm oder unter 0cm liegt,…
+    Serial.println("Kein Messwert"); //dann soll der serial monitor ausgeben „Kein Messwert“, weil Messwerte in diesen Bereichen falsch oder ungenau sind.
+  }
+  else {
+    Serial.print(entfernungLinks); //…soll der Wert der Entfernung an den serial monitor hier ausgegeben werden.
+    Serial.println(" cm Links"); // Hinter dem Wert der Entfernung soll auch am Serial Monitor die Einheit "cm" angegeben werden, danach eine neue Zeile
+  }
+}
+
+
+void entfernungMessenRechts() {
+  digitalWrite(TRIGGER_RECHTS, LOW); //Hier nimmt man die Spannung für kurze Zeit vom Trigger-Pin, damit man später beim Senden des Trigger-Signals ein rauschfreies Signal hat.
+  delay(5); // Pause 5 Millisekunden
+  digitalWrite(TRIGGER_RECHTS, HIGH); //Jetzt sendet man eine Ultraschallwelle los.
+  delay(10); //Dieser „Ton“ erklingt für 10 Millisekunden.
+  digitalWrite(TRIGGER_RECHTS, LOW);//Dann wird der „Ton“ abgeschaltet.
+  dauerRechts = pulseIn(ECHO_RECHTS, HIGH); //Mit dem Befehl „pulseIn“ zählt der Mikrokontroller die Zeit in Mikrosekunden, bis der Schall zum Ultraschallsensor zurückkehrt.
+  entfernungRechts = (long)((dauerRechts/2) * 0.03432); //Nun berechnet man die Entfernung in Zentimetern. Man teilt zunächst die Zeit durch zwei (Weil man ja nur eine Strecke berechnen möchte und nicht die Strecke hin- und zurück). Den Wert multipliziert man mit der Schallgeschwindigkeit in der Einheit Zentimeter/Mikrosekunde und erhält dann den Wert in Zentimetern.
+  
+  if (entfernungRechts >= 500 || entfernungRechts <= 0) {//Wenn die gemessene Entfernung über 500cm oder unter 0cm liegt,…
+    Serial.println("Kein Messwert"); //dann soll der serial monitor ausgeben „Kein Messwert“, weil Messwerte in diesen Bereichen falsch oder ungenau sind.
+  }
+  else {
+    Serial.print(entfernungRechts); //…soll der Wert der Entfernung an den serial monitor hier ausgegeben werden.
+    Serial.println(" cm Rechts"); // Hinter dem Wert der Entfernung soll auch am Serial Monitor die Einheit "cm" angegeben werden, danach eine neue Zeile
+  }
+}
+
+void umdrehungZeit() {
+    delay(900);                          //HIER ZEIT EINFÜGEN WIE LANG ES DAUERT FÜR EINE KURVE
+}
+
+void kursUmdrehungZeit() { //Zeit um wieder auf den Kurs zu kommen
+    delay(100);                          //HIER ZEIT EINFÜGEN WIE LANG ES DAUERT FÜR EINE UMDREHUNG
+}
+
+void halbUmdrehungRechts() { //Quasi 90* Drehung
+    outLeft = 200;
+    outRight = 0;
+    motorAnsteuern();
+    umdrehungZeit();
+    outLeft = 0;
+    motorAnsteuern();
+}
+
+void halbUmdrehungLinks() { //Quasi 90* Drehung
+    outLeft = 0;
+    outRight = 200;
+    motorAnsteuern();
+    umdrehungZeit();
+    outRight = 0;
+    motorAnsteuern();
+}
+
+// ------------------------------------------------------------------------------------
+// -                                Ende der Methoden                                 -
+// ------------------------------------------------------------------------------------
+
+
+
+// - Erster Start -
+
+void setup() {
+  Serial.begin(9600); //Starte den Serial Monitor
+  // Motor rechts
+  pinMode(RIGHT_RPWM,OUTPUT); //Pin-Modus setzen --> Pulsweitenmodulation
+  pinMode(RIGHT_LPWM,OUTPUT); //Pin-Modus setzen --> Pulsweitenmodulation
+  pinMode(RIGHT_LEN,OUTPUT);  //Pin-Modus setzen --> Pulsweitenmodulation
+  pinMode(RIGHT_REN,OUTPUT);  //Pin-Modus setzen --> Pulsweitenmodulation
+  digitalWrite(RIGHT_REN,HIGH); //Pin beschreiben
+  digitalWrite(RIGHT_LEN,HIGH); //Pin beschreiben
+  // Motor links
+  pinMode(LEFT_RPWM,OUTPUT); //Pin-Modus setzen --> Pulsweitenmodulation
+  pinMode(LEFT_LPWM,OUTPUT); //Pin-Modus setzen --> Pulsweitenmodulation
+  pinMode(LEFT_LEN,OUTPUT);  //Pin-Modus setzen --> Pulsweitenmodulation
+  pinMode(LEFT_REN,OUTPUT);  //Pin-Modus setzen --> Pulsweitenmodulation
+  digitalWrite(LEFT_REN,HIGH); //Pin beschreiben
+  digitalWrite(LEFT_LEN,HIGH); //Pin beschreiben
+  // Abstandssensor vorne
+  pinMode(TRIGGER_VORNE, OUTPUT); // Trigger-Pin ist ein Ausgang
+  pinMode(ECHO_VORNE, INPUT); // Echo-Pin ist ein Eingang
+  // Abstandssensor links
+  pinMode(TRIGGER_LINKS, OUTPUT); // Trigger-Pin ist ein Ausgang
+  pinMode(ECHO_LINKS, INPUT); // Echo-Pin ist ein Eingang
+  // Abstandssensor rechts
+  pinMode(TRIGGER_RECHTS, OUTPUT); // Trigger-Pin ist ein Ausgang
+  pinMode(ECHO_RECHTS, INPUT); // Echo-Pin ist ein Eingang
+
+  
+  Serial.println("--- Pins gesetzt");
+  fahrenBeide(); //Bot startet das Fahren
+  Serial.println("--- Im Setup Fahren gestartet");
+  delay(3000); //Delay dass nicht direkt irgendwelche Hindernisse erkannt werden
+}
+
+
+/*
+ * Main Loop
+ */
+void loop() {
+  entfernungMessenVorne(); // er misst durchgehend die entfernung nach vorne
+  delay(50); // delay nur zum testen da
+  if (entfernungVorne >= 12) { //wenn vorne eine wand ist dann fängt er an links und rechts zu messen
+    stehenbleiben(); //direkt stehenbleiben
+    entfernungMessenLinks(); 
+    delay(50); //zum testen da
+    entfernungMessenRechts();
+    delay(50);
+    if (entfernungLinks >= 17) { //wenn links eine wand ist wird hindernisLinks auf 1 gesetzt
+      hindernisLinks = 1;
+    }
+    if (entfernungRechts >= 17) { //wenn rechts eine wand ist wird hindernisRechts auf 1 gesetzt
+      hindernisRechts = 1;
+    }
+    //Abfrage start 
+    if (hindernisLinks + hindernisRechts) {
+      stehenbleiben();
+      Serial.println("Stehen geblieben, da 2 Hindernisse vorhanden sind");
+    }
+  }
+  delay(200); //Delay dass der ned durchdreht ;)
+}
+//Code Ende
