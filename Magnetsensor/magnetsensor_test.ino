d611ae2657cdbcf87aff1c109a46bc62953b656e
emaa10 <emanuelberger49@gmail.com>
2022-02-19 16:13:15 +0100

    fehler behoben

--- Magnetsensor/magnetsensor_test.ino
+++ Magnetsensor/magnetsensor_test.ino
@@ -4,18 +4,18 @@
 int magnetSensor = A0; // PIN für den Magnetischen Hall Sensors
  
 void setup () {
-  pinMode (mhSensor, INPUT);  // definieren des PIN's für den Sensor als Eingangssignal
+  pinMode (magnetSensor, INPUT);  // definieren des PIN's für den Sensor als Eingangssignal
   Serial.begin(9600);
 }
  
 void loop (){
-int val = digitalRead (mhSensor) ; // Lesen des Zustandes des Sensors.
-  if (val == LOW){ //Wenn dieser AN ist dann soll die StatusLed leuchten.
-    Serial.println("led high")
+int magnetSignal = digitalRead (magnetSensor) ; // Lesen des Zustandes des Sensors.
+  if (magnetSignal == LOW){ //Wenn dieser AN ist dann soll die StatusLed leuchten.
+    Serial.println("led high");
 //    digitalWrite (statusLed, HIGH);
   } 
   else { //Wenn dieser AUS ist dann soll die StatusLed NICHT leuchten.
 //    digitalWrite (statusLed, LOW);
-    Serial.println("led low")
+    Serial.println("led low");
   }
 }
