fcc614c7f5aa34711369d75f8b6914c442a0ba7b
emaa10 <emanuelberger49@gmail.com>
2022-02-19 14:31:46 +0100

    sus bus mabus

--- infrarot_magnet_test.ino
+++ infrarot_magnet_test.ino
@@ -1,22 +1,53 @@
-int ir_left = A3; // connect ir sensor to arduino pin 2 (left one)
+#define IR_LEFT = A3 // connect ir sensor to arduino pin 2 (left one)
+#define IR_MIDDLE = A2 // connect ir sensor to arduino pin 2 (middle one)
+#define IR_RIGHT = A1 // connect ir sensor to arduino pin 2 (middle one)
+int statusSensorLeft;
+int statusSensorMiddle;
+int statusSensorRight;
 
 void setup() 
 {
-  pinMode (ir_left, INPUT); // sensor pin INPUT
+  pinMode (IR_LEFT, INPUT); // sensor pin INPUT
+  pinMode (IR_MIDDLE, INPUT); // sensor pin INPUT
+  pinMode (IR_RIGHT, INPUT); // sensor pin INPUT
   Serial.begin(9600);
 }
 
+void linieLinks() {
+    statusSensorLeft = digitalRead (IR_LEFT);
+    if (statusSensorLeft == 1) {
+      Serial.println("Left kein Hindernis");
+    }
+    else {
+      Serial.println("Left Hindernis issda");
+    }
+}
+
+void linieMitte() {
+    statusSensorMiddle = digitalRead (IR_MIDDLE);
+    if (statusSensorMiddle == 1) {
+      Serial.println("Mitte kein Hindernis");
+    }
+    else {
+      Serial.println("Mitte Hindernis issda");
+    }
+}
+
+void linieRechts() {
+    statusSensorRight = digitalRead (IR_RIGHT);
+    if (statusSensorRight == 1) {
+      Serial.println("Rechts kein Hindernis");
+    }
+    else {
+      Serial.println("Rechts Hindernis issda");
+    }
+}
+
+
 void loop()
 {
-  int statusSensorLeft = digitalRead (ir_left);
-  
-  if (statusSensorLeft == 1) {
-    Serial.println("Left kein Hindernis");
-  }
-  
-  else
-  {
-    Serial.println("Left Hindernis issda");
-  }
+  linieLinks();
+  linieMitte();
+  linieRechts();
   delay(200);
 }
