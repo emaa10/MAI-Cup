7fce63b88ea904f7c862dacbfef10f43e9e64036
BuildTools <unconfigured@null.spigotmc.org>
2022-03-05 15:19:44 +0100

    sus nh alles überarbeitet usw

--- Liniensensor/lieniensensoren_christoph.ino
+++ Liniensensor/lieniensensoren_christoph.ino
@@ -256,52 +256,58 @@ void linieVerfolgen() {
 void umdrehungZeit() {
     delay(830);                          //HIER ZEIT EINFÜGEN WIE LANG ES DAUERT FÜR EINE KURVE
 }
-void linieLinks() { // Sensor links
+void linieLinks() { //Sensor links
   int statusSensorLeft = digitalRead(ir_left); //funktioniert
   if(statusSensorLeft == 1) { // diese abfrage kann man später auch noch verwenden
     Serial.print("Linie");
-    Serial.print("   "); // serial monitor bestätigt kann mit fahr code anfangen
-    outLeft = 0;//setz die Motoren auf die drehung
+    Serial.print("   "); //serial monitor bestätigt kann mit fahr code anfangen
+    outLeft = 0; //setz die Motoren auf die drehung
     outRight = 200;
     motorAnsteuern();
     for (int i=0; i <= 830; i++){ //startet die Schleife um sich um 90* zu drehen, macht dies 830 mal da in der schleife ein delay von 1 drin ist
-      delay(1); // das besagte delay
+      delay(1); //das besagte delay
       if (statusSensorRight == 1) {//check in der Schleife ob nicht der andere sensor die Linie erkennnt, denn das würde bedeuten das er dieser noch folgen müsste
         for (statusSensorLeft == 1) {//wenn eben dieser Fall eintritt dreht er sich wieder zurück auf die linie und da es im loop ist wird dadurch die linie auch verfolgt
-          outLeft = 200;//gibt den Motor das Signal sich wieder zurück zu drehen und weiter der Linie zu folgen
+          outLeft = 200; //gibt den Motor das Signal sich wieder zurück zu drehen und weiter der Linie zu folgen
           outRight = 0;
           motorAnsteuern(); 
         } 
       }
-      outLeft = 0;//er richtet sich wieder aus um die Linie gerade zu verfolgen
-      outRight = 200;
-      delay (30)
-        motorAnsteuern();
-        //delay(30);//Zeit der drehung zum wieder auf linie ausrichten idk
     }
-  else {
+    outLeft = 0; //er richtet sich wieder aus um die Linie gerade zu verfolgen
+    outRight = 200;
+    delay(30);
+    motorAnsteuern();
+    //delay(30); //Zeit der drehung zum wieder auf linie ausrichten idk
+  } else {
     Serial.print("Boden");
     Serial.print("   ");
   }
 
-void linieRechts() { // Sensor rechts
-  int statusSensorRight = digitalRead(ir_right);
-  if(statusSensorRight == 1) {
+void linieRechts() { //Sensor rechts
+  int statusSensorRight = digitalRead(ir_Right); //funktioniert jajaja
+  if(statusSensorRight == 1) { // diese abfrage kann man später auch noch verwenden
     Serial.print("Linie");
-    Serial.print("   ");
-    outLeft = 200;//setz die Motoren auf die drehung
-    outRight = 0;
-    for (830){//startet die Schleife um sich um 90* zu drehen, macht dies 830 mal da in der schleife ein delay von 1 drin ist
-        delay(1);// das besagte delay
-        if (statusSensorLeft == 1) {//check in der Schleife ob nicht der andere sensor die Linie erkennnt, denn das würde bedeuten das er dieser noch folgen müsste
-            for (statusSensorRight == 1) {//wenn eben dieser Fall eintritt dreht er sich wieder zurück auf die linie und da es im loop ist wird dadurch die linie auch verfolgt
-                outLeft = 0;//gibt den Motor das Signal sich wieder zurück zu drehen und weiter der Linie zu folgen
-                outRight = 200;}
-            outLeft =200;//er richtet sich wieder aus um die Linie gerade zu verfolgen
-            outRight = 0;
-            delay(30);//Zeit der drehung zum wieder auf linie ausrichten idk
-
-  } else
+    Serial.print("   "); //serial monitor bestätigt kann mit fahr code anfangen
+    outRight = 0; //setz die Motoren auf die drehung
+    outLeft = 200;
+    motorAnsteuern();
+    for (int i=0; i <= 830; i++){ //startet die Schleife um sich um 90* zu drehen, macht dies 830 mal da in der schleife ein delay von 1 drin ist
+      delay(1); //das besagte delay
+      if (statusSensorLeft == 1) {//check in der Schleife ob nicht der andere sensor die Linie erkennnt, denn das würde bedeuten das er dieser noch folgen müsste
+        for (statusSensorRight == 1) {//wenn eben dieser Fall eintritt dreht er sich wieder zurück auf die linie und da es im loop ist wird dadurch die linie auch verfolgt
+          outRight = 200; //gibt den Motor das Signal sich wieder zurück zu drehen und weiter der Linie zu folgen
+          outLeft = 0;
+          motorAnsteuern(); 
+        } 
+      }
+    }
+    outRight = 0; //er richtet sich wieder aus um die Linie gerade zu verfolgen
+    outLeft = 200;
+    delay(30);
+    motorAnsteuern();
+    //delay(30); //Zeit der drehung zum wieder auf linie ausrichten idk
+  } else {
     Serial.print("Boden");
     Serial.print("   ");
-}
+  }
