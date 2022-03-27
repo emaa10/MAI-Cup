606459561ae5eb2b0c40766b7b546ccb552f4925
BuildTools <unconfigured@null.spigotmc.org>
2022-03-27 16:45:16 +0200

    tests

--- LED und Port Expander/test_skript.ino
+++ LED und Port Expander/test_skript.ino
@@ -1,9 +1,11 @@
 #include "PCF8575.h"
 PCF8575 pcf8575(0x21, A0, A1);
-#define LED_PIN P0
+#define LED_PIN 0
 
 void setup() {
   Serial.begin(9600);
+  // Set pinMode to OUTPUT
+  pcf8575.pinMode(LED_PIN, OUTPUT);
   pcf8575.begin();
 }
 
