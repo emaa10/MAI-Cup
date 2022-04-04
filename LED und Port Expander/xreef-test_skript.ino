f3520b4ebf327877c8c87831c182540e17c06ace
BuildTools <unconfigured@null.spigotmc.org>
2022-04-04 19:51:26 +0200

    tests

--- LED und Port Expander/xreef-test_skript.ino
+++ LED und Port Expander/xreef-test_skript.ino
@@ -1,14 +1,15 @@
+#include "Arduino.h"
 #include "PCF8575.h"
 
-PCF8575(0x21);
+PCF8575 pcf8575(0x21);
 
 void setup() {
   pcf8575.pinMode(P0, OUTPUT);
 }
 
 void loop() {
-  PCF8575.digitalWrite(P0, HIGH);
+  pcf8575.digitalWrite(P0, HIGH);
   delay(1000);
-  PCF8575.digitalWrite(P0, LOW);
+  pcf8575.digitalWrite(P0, LOW);
   delay(1000);
 }
