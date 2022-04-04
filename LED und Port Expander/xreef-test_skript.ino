b0fb217fbf077ecbe38f3eb1be74d44f2e8cf9c7
BuildTools <unconfigured@null.spigotmc.org>
2022-04-04 19:40:36 +0200

    erster test code geschrieben

--- LED und Port Expander/xreef-test_skript.ino
+++ LED und Port Expander/xreef-test_skript.ino
@@ -1,3 +1,14 @@
 #include "PCF8575.h"
 
 PCF8575(0x21);
+
+void setup() {
+  pcf8575.pinMode(P0, OUTPUT);
+}
+
+void loop() {
+  PCF8575.digitalWrite(P0, HIGH);
+  delay(1000);
+  PCF8575.digitalWrite(P0, LOW);
+  delay(1000);
+}
