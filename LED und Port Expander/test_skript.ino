92c121f7e9d1af52fec5b6a45569b704861e0af5
emaa10 <emanuelberger49@gmail.com>
2022-04-30 16:52:50 +0200

    tests

--- LED und Port Expander/test_skript.ino
+++ LED und Port Expander/test_skript.ino
@@ -2,16 +2,27 @@
 
 PCF8575 pcf8575(0x21);
 #define LED_PIN P0
+#define MAGNETPIN P7
 
 void setup() {
   Serial.begin(9600);
   // Set pinMode to OUTPUT
   pcf8575.pinMode(LED_PIN, OUTPUT);
+  pcf8575.pinMode(MAGNETPIN, INPUT);     //wie normal, wir geben etwas in den mikrocontroller ein, daher input
   pcf8575.begin();
 }
 
 void loop() {
+  pcf8575.digitalWrite(LED_PIN, LOW);
+  Serial.println("LED Pin lo");
+  delay(1000);
   pcf8575.digitalWrite(LED_PIN, HIGH);
   Serial.println("LED Pin high");
   delay(1000);
+  
+  //magnetsensor
+  
+  int magnet=pcf8575.digitalRead(MAGNETPIN);
+  Serial.println(magnet);
+
 }
